f595f017a5530ceea5f5bd1e483a3ac46d45b046
Alex <alexandrareginawilson@gmail.com>
2019-03-27 17:03:47 -0500

    reverting to older version where concert, movie and spotify work. need to finish do what it says from here.

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 
 var keys = require("./keys.js");
@@ -17,10 +16,11 @@ var axios = require("axios");
 
 
 var command = process.argv[2];
-var query = process.argv.slice(3);
+var query;
 
-function goLiri (command, query) {
+function goLiri () {
     if (command === "concert-this"){
+        var query = process.argv.slice(3);
         var modifiedQuery = query.join("+");
         var queryUrl = "https://rest.bandsintown.com/artists/" + modifiedQuery + "/events?app_id=codingbootcamp";
         axios.get(queryUrl).then(
@@ -33,34 +33,34 @@ function goLiri (command, query) {
                 console.log(error);
             })
     } else if (command === "movie-this"){
-        if (!query.length > 0){
+        if (!process.argv[3]){
             axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
                 function(response){
-                    console.log("Title: " + response.data.Title);
-                    console.log("Released in: " + response.data.Year);
-                    console.log("IMDB rating: " + response.data.Ratings[0].Value);
-                    console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
-                    console.log("Country: " + response.data.Country);
-                    console.log("Language: " + response.data.Language);
-                    console.log("Plot: " + response.data.Plot);
-                    console.log("Actors: " + response.data.Actors);
+                console.log("Title: " + response.data.Title);
+                console.log("Released in: " + response.data.Year);
+                console.log("IMDB rating: " + response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
             }).catch(function(error) {
                 console.log(error);
             })
         } else {
-            // var query = process.argv.slice(3);
+            var query = process.argv.slice(3);
             var modifiedQuery = query.join("+");
             var queryUrl = "http://www.omdbapi.com/?t=" + modifiedQuery + "&y=&plot=short&apikey=trilogy";
             axios.get(queryUrl).then(
                 function(response){
-                    console.log("Title: " + response.data.Title);
-                    console.log("Released in: " + response.data.Year);
-                    console.log("IMDB rating: " + response.data.Ratings[0].Value);
-                    console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
-                    console.log("Country: " + response.data.Country);
-                    console.log("Language: " + response.data.Language);
-                    console.log("Plot: " + response.data.Plot);
-                    console.log("Actors: " + response.data.Actors);
+                console.log("Title: " + response.data.Title);
+                console.log("Released in: " + response.data.Year);
+                console.log("IMDB rating: " + response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
             }).catch(function(error) {
                 console.log(error);
             })
@@ -79,6 +79,7 @@ function goLiri (command, query) {
                 console.log(error);
             })
         } else {
+            var query = String(process.argv.slice(3));
             spotify
             .search({ type: 'track', query: query, limit: 1 })
             .then(function(response) {
@@ -99,10 +100,10 @@ function goLiri (command, query) {
                 var dataArr = data.split(",");
                 command = dataArr[0];
                 query = dataArr[1];
-                goLiri(command,query.split(" "));
+                goLiri();
             }
         })
     }
 }
 
-goLiri(command,query);
+goLiri();
