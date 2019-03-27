1dea6e0d519741bd255386948860c890683a1d78
Alex <alexandrareginawilson@gmail.com>
2019-03-27 18:23:58 -0500

    fixing stray parentheses and brackets

--- liri.js
+++ liri.js
@@ -90,9 +90,9 @@ function goLiri () {
             })
             .catch(function(error) {
                 console.log(error);
-            });
+            })
         }
-    } else if (command === "do-what-it-says"){
+    } else if (command === "do-what-it-says") {
         fs.readFile("random.txt", "utf8", function(error, data) {
             if (error) {
                 return console.log(error);
@@ -100,12 +100,9 @@ function goLiri () {
                 var dataArr = data.split(",");
                 var newCommand = dataArr[0];
                 var newQuery = dataArr[1].split(" ");
-                console.log(newCommand, newQuery);
                 if (newCommand === "concert-this"){
                     var newModifiedQuery = newQuery.join("+");
-                    console.log(newModifiedQuery);
                     var queryUrl = "https://rest.bandsintown.com/artists/" + newModifiedQuery + "/events?app_id=codingbootcamp";
-                    console.log(queryUrl);
                     axios.get(queryUrl).then(
                         function(response){
                             console.log("Venue: " + response.data[0].venue.name);
@@ -116,23 +113,7 @@ function goLiri () {
                               console.log(error);
                         })
                 } else if (newCommand === "movie-this"){
-                    // if (!newQuery){
-                    //     axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
-                    //         function(response){
-                    //             console.log("Title: " + response.data.Title);
-                    //             console.log("Released in: " + response.data.Year);
-                    //             console.log("IMDB rating: " + response.data.Ratings[0].Value);
-                    //             console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
-                    //             console.log("Country: " + response.data.Country);
-                    //             console.log("Language: " + response.data.Language);
-                    //             console.log("Plot: " + response.data.Plot);
-                    //             console.log("Actors: " + response.data.Actors);
-                    //     }).catch(function(error) {
-                    //           console.log(error);
-                    //     })
-                    // } else {
                         var newModifiedQuery = newQuery.join("+");
-                        console.log(newModifiedQuery);
                         var queryUrl = "http://www.omdbapi.com/?t=" + newModifiedQuery + "&y=&plot=short&apikey=trilogy";
                         axios.get(queryUrl).then(
                             function(response){
@@ -148,35 +129,22 @@ function goLiri () {
                               console.log(error);
                         })
                   } else if (newCommand === "spotify-this-song"){
-                        if (!newQuery){
-                            spotify
-                            .search({ type: 'track', query: 'The Sign Ace of Base', limit: 1 })
-                            .then(function(response) {
-                                console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
-                                console.log("Song: " + response.tracks.items[0].name);
-                                console.log("Album: " + response.tracks.items[0].album.name);
-                                console.log("Snippet: " + response.tracks.items[0].preview_url);
-                            })
-                            .catch(function(error) {
-                                console.log(error);
-                            })
-                        } else {
-                            spotify
-                            .search({ type: 'track', query: newQuery, limit: 1 })
-                            .then(function(response) {
-                                console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
-                                console.log("Song: " + response.tracks.items[0].name);
-                                console.log("Album: " + response.tracks.items[0].album.name);
-                                console.log("Snippet: " + response.tracks.items[0].preview_url);
-                            })
-                            .catch(function(error) {
-                                console.log(error);
-                            });
-                        }
+                        spotify
+                        .search({ type: 'track', query: newQuery, limit: 1 })
+                        .then(function(response) {
+                            console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
+                            console.log("Song: " + response.tracks.items[0].name);
+                            console.log("Album: " + response.tracks.items[0].album.name);
+                            console.log("Snippet: " + response.tracks.items[0].preview_url);
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+                  }
+    
                 }
-            }
-        })
-    }
+                }              
+        )}
+        
 }
-
 goLiri();
